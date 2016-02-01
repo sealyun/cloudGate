00ba5e45f648ad90370c1fb0d1aeb67a5b8ea295
fanux <474785153@qq.com>
2016-02-01 15:41:33 +0800

    complete image handlers

--- modules/image_service/handlers.py
+++ modules/image_service/handlers.py
@@ -10,25 +10,221 @@ class ImageBaseHandler(HttpBaseHandler):
 
 class ImagesHandler(ImageBaseHandler):
     def get(self):
-        pass
+        limit = self.get_argument("limit", None)
+        marker = self.get_argument("marker", None)
+        name = self.get_argument("name", None)
+        visibility = self.get_argument("visibility", None)
+        member_status = self.get_argument("member_status", None)
+        owner = self.get_argument("owner", None)
+        status = self.get_argument("status", None)
+        size_min = self.get_argument("size_min", None)
+        size_max = self.get_argument("size_max", None)
+        sort_key = self.get_argument("sort_key", None)
+        sort_dir = self.get_argument("sort_dir", None)
+        sort = self.get_argument("sort", None)
+        tag = self.get_argument("tag", None)
+
+        images = self.p.queryImages(limit, marker, name, visibility, member_status, owner, status,
+                size_min, size_max, sort_key, sort_dir, sort, tag)
+
+        resp = {
+            "images":[
+                {
+                    "status":i.status,
+                    "name":i.name,
+                    "tags":i.tags,
+                    "container_format":i.container_format,
+                    "create_at":i.create_at,
+                    "disk_format":i.disk_format,
+                    "updated_at":i.updated_at,
+                    "visibility": i.visibility,
+                    "self": "/v2/images/"+i.id,
+                    "min_disk": i.min_disk,
+                    "protected": i.protected,
+                    "id": i.id,
+                    "file": "/v2/images/"+i.id+"/file",
+                    "checksum": i.checksum,
+                    "owner": i.owner,
+                    "size": i.size,
+                    "min_ram": i.min_ram,
+                    "schema": i.schema,
+                    "virtual_size": i.virtual_size 
+                }
+                for i in images
+            ]
+        }
+
+        self.send_json(resp)
 
     def post(self):
-        pass
+        image = json.loads(self.request.body)
+        i = self.p.createImage(image["container_format"],
+                image["disk_format"],
+                image["name"],
+                image["id"])
+
+        if !i:
+            return
+
+        resp = {
+            "status":i.status,
+            "name":i.name,
+            "tags":i.tags,
+            "container_format":i.container_format,
+            "create_at":i.create_at,
+            "size": i.size,
+            "disk_format":i.disk_format,
+            "updated_at":i.updated_at,
+            "visibility": i.visibility,
+            "locations":i.locations,
+            "self": "/v2/images/"+i.id,
+            "min_disk": i.min_disk,
+            "protected": i.protected,
+            "id": i.id,
+            "file": "/v2/images/"+i.id+"/file",
+            "checksum": i.checksum,
+            "owner": i.owner,
+            "virtual_size": i.virtual_size 
+            "min_ram": i.min_ram,
+            "schema": i.schema,
+        }
+
+        self.set_status(201)
+        self.send_json(resp)
 
 class ImageHandler(ImageBaseHandler):
     def get(self, image_id):
-        pass
+        i = self.p.queryImageId(image_id)
+        if !i:
+            return
+
+        resp = {
+            "status":i.status,
+            "name":i.name,
+            "tags":i.tags,
+            "container_format":i.container_format,
+            "create_at":i.create_at,
+            "disk_format":i.disk_format,
+            "updated_at":i.updated_at,
+            "visibility": i.visibility,
+            "self": "/v2/images/"+i.id,
+            "min_disk": i.min_disk,
+            "protected": i.protected,
+            "id": i.id,
+            "file": "/v2/images/"+i.id+"/file",
+            "checksum": i.checksum,
+            "owner": i.owner,
+            "size": i.size,
+            "min_ram": i.min_ram,
+            "schema": i.schema,
+            "virtual_size": i.virtual_size 
+        }
+
+        self.send_json(resp)
 
     def patch(self, image_id):
-        pass
+        update_list = json.laods(self.request.body)
+        #"op" "path" "value"
+
+        i = updateImage(image_id, update_list)
+
+        resp = {
+            "id":i.id,
+            "name":i.name,
+            "status":i.status,
+            "visibility": i.visibility,
+            "size": i.size,
+            "checksum": i.checksum,
+            "tags":i.tags,
+            "create_at":i.create_at,
+            "updated_at":i.updated_at,
+            "self": "/v2/images/"+i.id,
+            "file": "/v2/images/"+i.id+"/file",
+            "schema": i.schema,
+            "owner": i.owner,
+            "min_ram": i.min_ram,
+            "min_disk": i.min_disk,
+            "disk_format":i.disk_format,
+            "virtual_size": i.virtual_size
+            "container_format":i.container_format,
+        }
+
+        self.send_json(resp)
+
 
     def delete(self, image_id):
-        pass
+        res = self.p.deleteImage(image_id)
+
+        if res:
+            self.set_status(204)
+        else:
+            self.set_status(403)
 
 class ImageActionReactivateHandler(ImageBaseHandler):
     def post(self, image_id):
-        pass
+        i = self.p.reactivateImage(image_id):
+        if i:
+            self.set_status(204)
+        else:
+            return
 
+        resp = {
+            "status":i.status,
+            "name":i.name,
+            "tags":i.tags,
+            "container_format":i.container_format,
+            "create_at":i.create_at,
+            "disk_format":i.disk_format,
+            "updated_at":i.updated_at,
+            "visibility": i.visibility,
+            "self": "/v2/images/"+i.id,
+            "min_disk": i.min_disk,
+            "protected": i.protected,
+            "id": i.id,
+            "file": "/v2/images/"+i.id+"/file",
+            "checksum": i.checksum,
+            "owner": i.owner,
+            "size": i.size,
+            "min_ram": i.min_ram,
+            "schema": i.schema,
+            "virtual_size": i.virtual_size 
+        }
+
+        self.send_json(resp)
+
+class ImageActionDeactivateHandler(ImageBaseHandler):
+    def post(self, image_id):
+        i = self.p.deactivateImage(image_id):
+        if i:
+            self.set_status(204)
+        else:
+            return
+
+        resp = {
+            "status":i.status,
+            "name":i.name,
+            "tags":i.tags,
+            "container_format":i.container_format,
+            "create_at":i.create_at,
+            "disk_format":i.disk_format,
+            "updated_at":i.updated_at,
+            "visibility": i.visibility,
+            "self": "/v2/images/"+i.id,
+            "min_disk": i.min_disk,
+            "protected": i.protected,
+            "id": i.id,
+            "file": "/v2/images/"+i.id+"/file",
+            "checksum": i.checksum,
+            "owner": i.owner,
+            "size": i.size,
+            "min_ram": i.min_ram,
+            "schema": i.schema,
+            "virtual_size": i.virtual_size 
+        }
+
+        self.send_json(resp)
+
+#TODO need upload download binary file
 class ImageFileHandler(ImageBaseHandler):
     def put(self, image_id):
         pass
--- modules/image_service/url.py
+++ modules/image_service/url.py
@@ -4,6 +4,7 @@ urls = [
     (IMAGE_SERVICE_BASE_URL, ImageBaseHandler),
     (IMAGE_SERVICE_BASE_URL + r"/v2/images", ImagesHandler),
     (IMAGE_SERVICE_BASE_URL + r"/v2/images/(.*)", ImageHandler),
-    (IMAGE_SERVICE_BASE_URL + r"/v2/images/(.*)/action/reactivate", ImageActionReactivateHandler),
+    (IMAGE_SERVICE_BASE_URL + r"/v2/images/(.*)/actions/reactivate", ImageActionReactivateHandler),
+    (IMAGE_SERVICE_BASE_URL + r"/v2/images/(.*)/actions/deactivate", ImageActionDeactivateHandler),
     (IMAGE_SERVICE_BASE_URL + r"/v2/images/(.*)/file", ImageFileHandler), 
 ]
