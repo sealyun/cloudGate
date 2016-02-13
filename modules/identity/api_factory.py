2c0495315efe835fa31b8bb24e80529f735645ed
fanux <474785153@qq.com>
2016-02-13 15:09:38 +0800

    add auth

--- modules/identity/api_factory.py
+++ modules/identity/api_factory.py
@@ -3,5 +3,5 @@ from aliyun.processor import *
 class IdentityProcessorFac():
 
     def __init__(self, type_ = None, token = None):
-        if type_ == "aliyun" or !type_:
+        if type_ == "aliyun" or not type_:
             return AliyunIdentityProcessor(token)
