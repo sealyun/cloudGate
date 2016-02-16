d04f61bc4dc1a6d14f007b4cb5ca3b0329bd1332
= <mohanson@163.com>
2016-02-16 15:33:27 +0800

    ywb

--- modules/image_service/handlers.py
+++ modules/image_service/handlers.py
@@ -2,20 +2,23 @@ from tornado.gen import coroutine
 from cloudGate.httpbase import HttpBaseHandler
 from api_factory import ImageServiceProcessorFac
 
+
 class ImageBaseHandler(HttpBaseHandler):
+
     def get_processor(self):
         token = self.request.headers["X-Auth-Token"]
-        print ("-----get token:", token)
+        print("-----get token:", token)
         i = ImageServiceProcessorFac()
         self.p = i.create_processor(None, token)
 
         return self.p
 
-
     def get(self):
         pass
 
+
 class ImagesHandler(ImageBaseHandler):
+
     def get(self):
         limit = self.get_argument("limit", None)
         marker = self.get_argument("marker", None)
@@ -33,30 +36,30 @@ class ImagesHandler(ImageBaseHandler):
 
         self.get_processor()
         images = self.p.queryImages(limit, marker, name, visibility, member_status, owner, status,
-                size_min, size_max, sort_key, sort_dir, sort, tag)
+                                    size_min, size_max, sort_key, sort_dir, sort, tag)
 
         resp = {
-            "images":[
+            "images": [
                 {
-                    "status":i.status,
-                    "name":i.name,
-                    "tags":i.tags,
-                    "container_format":i.container_format,
-                    "create_at":i.create_at,
-                    "disk_format":i.disk_format,
-                    "updated_at":i.updated_at,
-                    "visibility": i.visibility,
-                    "self": "/v2/images/"+i.id,
-                    "min_disk": i.min_disk,
-                    "protected": i.protected,
-                    "id": i.id,
-                    "file": "/v2/images/"+i.id+"/file",
-                    "checksum": i.checksum,
-                    "owner": i.owner,
-                    "size": i.size,
-                    "min_ram": i.min_ram,
-                    "schema": i.schema,
-                    "virtual_size": i.virtual_size
+                    "status": i['Status'],
+                    "name": i['ImageName'],
+                    "tags": [],
+                    "container_format": '',
+                    "create_at": i['CreationTime'],
+                    "disk_format": '',
+                    "updated_at": '',
+                    "visibility": i['IsSubscribed'],
+                    "self": "/v2/images/" + i['ImageId'],
+                    "min_disk": 0,
+                    "protected": i['IsCopied'],
+                    "id": i['ImageId'],
+                    "file": "/v2/images/" + i['ImageId'] + "/file",
+                    "checksum": None,
+                    "owner": i['ImageOwnerAlias'],
+                    "size": i['Size'],
+                    "min_ram": None,
+                    "schema": '',
+                    "virtual_size": i['Size']
                 }
                 for i in images
             ]
@@ -67,59 +70,61 @@ class ImagesHandler(ImageBaseHandler):
     def post(self):
         image = json.loads(self.request.body)
         i = self.p.createImage(image["container_format"],
-                image["disk_format"],
-                image["name"],
-                image["id"])
+                               image["disk_format"],
+                               image["name"],
+                               image["id"])
 
         if not i:
             return
 
         resp = {
-            "status":i.status,
-            "name":i.name,
-            "tags":i.tags,
-            "container_format":i.container_format,
-            "create_at":i.create_at,
+            "status": i.status,
+            "name": i.name,
+            "tags": i.tags,
+            "container_format": i.container_format,
+            "create_at": i.create_at,
             "size": i.size,
-            "disk_format":i.disk_format,
-            "updated_at":i.updated_at,
+            "disk_format": i.disk_format,
+            "updated_at": i.updated_at,
             "visibility": i.visibility,
-            "locations":i.locations,
-            "self": "/v2/images/"+i.id,
+            "locations": i.locations,
+            "self": "/v2/images/" + i.id,
             "min_disk": i.min_disk,
             "protected": i.protected,
             "id": i.id,
-            "file": "/v2/images/"+i.id+"/file",
+            "file": "/v2/images/" + i.id + "/file",
             "checksum": i.checksum,
             "owner": i.owner,
-            "virtual_size": i.virtual_size
-            # "min_ram": i.min_ram,
-            # "schema": i.schema,
+            "virtual_size": i.virtual_size,
+            "min_ram": i.min_ram,
+            "schema": i.schema,
         }
 
         self.set_status(201)
         self.send_json(resp)
 
+
 class ImageHandler(ImageBaseHandler):
+
     def get(self, image_id):
         i = self.p.queryImageId(image_id)
         if not i:
             return
 
         resp = {
-            "status":i.status,
-            "name":i.name,
-            "tags":i.tags,
-            "container_format":i.container_format,
-            "create_at":i.create_at,
-            "disk_format":i.disk_format,
-            "updated_at":i.updated_at,
+            "status": i.status,
+            "name": i.name,
+            "tags": i.tags,
+            "container_format": i.container_format,
+            "create_at": i.create_at,
+            "disk_format": i.disk_format,
+            "updated_at": i.updated_at,
             "visibility": i.visibility,
-            "self": "/v2/images/"+i.id,
+            "self": "/v2/images/" + i.id,
             "min_disk": i.min_disk,
             "protected": i.protected,
             "id": i.id,
-            "file": "/v2/images/"+i.id+"/file",
+            "file": "/v2/images/" + i.id + "/file",
             "checksum": i.checksum,
             "owner": i.owner,
             "size": i.size,
@@ -137,28 +142,27 @@ class ImageHandler(ImageBaseHandler):
         i = updateImage(image_id, update_list)
 
         resp = {
-            "id":i.id,
-            "name":i.name,
-            "status":i.status,
+            "id": i.id,
+            "name": i.name,
+            "status": i.status,
             "visibility": i.visibility,
             "size": i.size,
             "checksum": i.checksum,
-            "tags":i.tags,
-            "create_at":i.create_at,
-            "updated_at":i.updated_at,
-            "self": "/v2/images/"+i.id,
-            "file": "/v2/images/"+i.id+"/file",
+            "tags": i.tags,
+            "create_at": i.create_at,
+            "updated_at": i.updated_at,
+            "self": "/v2/images/" + i.id,
+            "file": "/v2/images/" + i.id + "/file",
             "schema": i.schema,
             "owner": i.owner,
             "min_ram": i.min_ram,
             "min_disk": i.min_disk,
-            "disk_format":i.disk_format,
+            "disk_format": i.disk_format,
             "virtual_size": i.virtual_size
             # "container_format":i.container_format,
         }
 
         self.send_json(resp)
-
 
     def delete(self, image_id):
         res = self.p.deleteImage(image_id)
@@ -168,7 +172,9 @@ class ImageHandler(ImageBaseHandler):
         else:
             self.set_status(403)
 
+
 class ImageActionReactivateHandler(ImageBaseHandler):
+
     def post(self, image_id):
         i = self.p.reactivateImage(image_id)
         if i:
@@ -177,19 +183,19 @@ class ImageActionReactivateHandler(ImageBaseHandler):
             return
 
         resp = {
-            "status":i.status,
-            "name":i.name,
-            "tags":i.tags,
-            "container_format":i.container_format,
-            "create_at":i.create_at,
-            "disk_format":i.disk_format,
-            "updated_at":i.updated_at,
+            "status": i.status,
+            "name": i.name,
+            "tags": i.tags,
+            "container_format": i.container_format,
+            "create_at": i.create_at,
+            "disk_format": i.disk_format,
+            "updated_at": i.updated_at,
             "visibility": i.visibility,
-            "self": "/v2/images/"+i.id,
+            "self": "/v2/images/" + i.id,
             "min_disk": i.min_disk,
             "protected": i.protected,
             "id": i.id,
-            "file": "/v2/images/"+i.id+"/file",
+            "file": "/v2/images/" + i.id + "/file",
             "checksum": i.checksum,
             "owner": i.owner,
             "size": i.size,
@@ -200,7 +206,9 @@ class ImageActionReactivateHandler(ImageBaseHandler):
 
         self.send_json(resp)
 
+
 class ImageActionDeactivateHandler(ImageBaseHandler):
+
     def post(self, image_id):
         i = self.p.deactivateImage(image_id)
         if i:
@@ -209,19 +217,19 @@ class ImageActionDeactivateHandler(ImageBaseHandler):
             return
 
         resp = {
-            "status":i.status,
-            "name":i.name,
-            "tags":i.tags,
-            "container_format":i.container_format,
-            "create_at":i.create_at,
-            "disk_format":i.disk_format,
-            "updated_at":i.updated_at,
+            "status": i.status,
+            "name": i.name,
+            "tags": i.tags,
+            "container_format": i.container_format,
+            "create_at": i.create_at,
+            "disk_format": i.disk_format,
+            "updated_at": i.updated_at,
             "visibility": i.visibility,
-            "self": "/v2/images/"+i.id,
+            "self": "/v2/images/" + i.id,
             "min_disk": i.min_disk,
             "protected": i.protected,
             "id": i.id,
-            "file": "/v2/images/"+i.id+"/file",
+            "file": "/v2/images/" + i.id + "/file",
             "checksum": i.checksum,
             "owner": i.owner,
             "size": i.size,
@@ -232,8 +240,11 @@ class ImageActionDeactivateHandler(ImageBaseHandler):
 
         self.send_json(resp)
 
-#TODO need upload download binary file
+# TODO need upload download binary file
+
+
 class ImageFileHandler(ImageBaseHandler):
+
     def put(self, image_id):
         pass
 
