541828ec7d6b03c53fbb002bc8e4c24dd009b166
= <mohanson@163.com>
2016-02-16 16:21:50 +0800

    i

--- modules/image_service/handlers.py
+++ modules/image_service/handlers.py
@@ -86,6 +86,25 @@ class ImagesHandler(ImageBaseHandler):
                         "distro": "Ubuntu 10.04 LTS"
                     }
                 },
+                {
+                    "uri": "http://glance.example.com/images/71c675ab-d94f-49cd-a114-e12490b328d9",
+                    "name": "Ubuntu 10.04 Plain 5GB",
+                    "disk_format": "vhd",
+                    "container_format": "ovf",
+                    "size": "5368709120",
+                    "checksum": "c2e5db72bd7fd153f53ede5da5a06de3",
+                    "created_at": "2010-02-03 09:34:01",
+                    "updated_at": "2010-02-03 09:34:01",
+                    "deleted_at": "",
+                    "status": "active",
+                    "is_public": True,
+                    "min_ram": 256,
+                    "min_disk": 5,
+                    "owner": None,
+                    "properties": {
+                        "distro": "Ubuntu 10.04 LTS"
+                    }
+                },
             ]
         })
 
