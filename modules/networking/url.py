82dd8f1d85b407d7a0fdbf5c61cfcb18d9ee9274
yjl <yjl@impower-tech.com>
2016-02-17 16:32:45 +0800

    h

--- modules/networking/url.py
+++ modules/networking/url.py
@@ -5,28 +5,28 @@ urls = [
     (NETWORKING_BASE_URL, NetworkingBaseHandler),
     (NETWORKING_BASE_URL + r"/v2.0/networks.json", NetworksHandler),
     (NETWORKING_BASE_URL + r"/v2.0/extensions.json", NetworksExtensionsHandler),
-    (NETWORKING_BASE_URL + r"/v2.0/networks/(.*).json", NetworkHandler),
-    (NETWORKING_BASE_URL + r"/v2.0/networks/(.*)/dhcp-agents.json", DHCPAgentsHandler),
+    (NETWORKING_BASE_URL + r"/v2.0/networks/(^[a-zA-Z0-9\.@\-_]+$).json", NetworkHandler),
+    (NETWORKING_BASE_URL + r"/v2.0/networks/(^[a-zA-Z0-9\.@\-_]+$)/dhcp-agents.json", DHCPAgentsHandler),
 
     (NETWORKING_BASE_URL + r"/v2.0/subnets.json", SubnetsHandler),
-    (NETWORKING_BASE_URL + r"/v2.0/subnets/(.*).json", SubnetHandler),
+    (NETWORKING_BASE_URL + r"/v2.0/subnets/(^[a-zA-Z0-9\.@\-_]+$).json", SubnetHandler),
 
     (NETWORKING_BASE_URL + r"/v2.0/ports.json", PortsHandler),
-    (NETWORKING_BASE_URL + r"/v2.0/ports/(.*).json", PortHandler),
+    (NETWORKING_BASE_URL + r"/v2.0/ports/(^[a-zA-Z0-9\.@\-_]+$).json", PortHandler),
 
     (NETWORKING_BASE_URL + r"/v2.0/lbaas/loadbalancers", LoadbalancersHandler),
-    (NETWORKING_BASE_URL + r"/v2.0/lbaas/loadbalancers/(.*)", LoadbalancerHandler),
-    (NETWORKING_BASE_URL + r"/v2.0/lbaas/loadbalancers/(.*)/statuses", LoadbalancerStatusesHandler),
+    (NETWORKING_BASE_URL + r"/v2.0/lbaas/loadbalancers/(^[a-zA-Z0-9\.@\-_]+$)", LoadbalancerHandler),
+    (NETWORKING_BASE_URL + r"/v2.0/lbaas/loadbalancers/(^[a-zA-Z0-9\.@\-_]+$)/statuses", LoadbalancerStatusesHandler),
 
     (NETWORKING_BASE_URL + r"/v2.0/lbaas/listeners", LbaasListenersHandler),
-    (NETWORKING_BASE_URL + r"/v2.0/lbaas/listeners/(.*)", LbaasListenerHandler),
+    (NETWORKING_BASE_URL + r"/v2.0/lbaas/listeners/(^[a-zA-Z0-9\.@\-_]+$)", LbaasListenerHandler),
 
     (NETWORKING_BASE_URL + r"/v2.0/lbaas/pools", LbaasPoolsHandler),
-    (NETWORKING_BASE_URL + r"/v2.0/lbaas/pools/(.*)", LbaasPoolHandler),
-    (NETWORKING_BASE_URL + r"/v2.0/lbaas/pools/(.*)/members", LbaasPoolMembersHandler),
-    (NETWORKING_BASE_URL + r"/v2.0/lbaas/pools/(.*)/members/(.*)", LbaasPoolMemberHandler),
+    (NETWORKING_BASE_URL + r"/v2.0/lbaas/pools/(^[a-zA-Z0-9\.@\-_]+$)", LbaasPoolHandler),
+    (NETWORKING_BASE_URL + r"/v2.0/lbaas/pools/(^[a-zA-Z0-9\.@\-_]+$)/members", LbaasPoolMembersHandler),
+    (NETWORKING_BASE_URL + r"/v2.0/lbaas/pools/(^[a-zA-Z0-9\.@\-_]+$)/members/(^[a-zA-Z0-9\.@\-_]+$)", LbaasPoolMemberHandler),
 
     (NETWORKING_BASE_URL + r"/v2.0/lbaas/health_monitors", LbaasHealthMonitorsHandler),
-    (NETWORKING_BASE_URL + r"/v2.0/lbaas/health_monitor/(.*)", LbaasHealthMonitorHandler),
+    (NETWORKING_BASE_URL + r"/v2.0/lbaas/health_monitor/(^[a-zA-Z0-9\.@\-_]+$)", LbaasHealthMonitorHandler),
 ]
 
