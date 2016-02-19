c24c03bf2e7164d0352b06f4b120bb557aba068d
wujun <nujuw@126.com>
2016-02-19 21:19:09 +0800

    update code ...... 1

--- modules/block_storage/url.py
+++ modules/block_storage/url.py
@@ -7,24 +7,24 @@ urls_low_version = [
 
 urls_v2 = [
     (BLOCK_STORAGE_BASE_URL + r"/v2", BlockStorageBaseHandler),
-    (BLOCK_STORAGE_BASE_URL + r"/v2/(.*)/volumes", VolumesHandler),
-    (BLOCK_STORAGE_BASE_URL + r"/v2/(.*)/volumes/detail", VolumesDetailHandler),
-    (BLOCK_STORAGE_BASE_URL + r"/v2/(.*)/volumes/(.*)", VolumeHandler),
-    (BLOCK_STORAGE_BASE_URL + r"/v2/(.*)/volumes/(.*)/metadata", VolumeMetadataHandler),
-    (BLOCK_STORAGE_BASE_URL + r"/v2/(.*)/volumes/(.*)/action", VolumeActionHandler),
+    (BLOCK_STORAGE_BASE_URL + r"/v2/([^/]+)/volumes", VolumesHandler),
+    (BLOCK_STORAGE_BASE_URL + r"/v2/([^/]+)/volumes/detail", VolumesDetailHandler),
+    (BLOCK_STORAGE_BASE_URL + r"/v2/([^/]+)/volumes/([^/]+)", VolumeHandler),
+    (BLOCK_STORAGE_BASE_URL + r"/v2/([^/]+)/volumes/([^/]+)/metadata", VolumeMetadataHandler),
+    (BLOCK_STORAGE_BASE_URL + r"/v2/([^/]+)/volumes/([^/]+)/action", VolumeActionHandler),
 
-    (BLOCK_STORAGE_BASE_URL + r"/v2/(.*)/snapshots", SnapshotsHandler),
-    (BLOCK_STORAGE_BASE_URL + r"/v2/(.*)/snapshots/detail", SnapshotsDetailHandler),
-    (BLOCK_STORAGE_BASE_URL + r"/v2/(.*)/snapshots/(.*)", SnapshotHandler),
-    (BLOCK_STORAGE_BASE_URL + r"/v2/(.*)/snapshots/(.*)/metadata", SnapshotMetadataHandler),
-    ## (BLOCK_STORAGE_BASE_URL + r"/v2/(.*)/snapshots/(.*)/action", SnapshotsActionHandler),
+    (BLOCK_STORAGE_BASE_URL + r"/v2/([^/]+)/snapshots", SnapshotsHandler),
+    (BLOCK_STORAGE_BASE_URL + r"/v2/([^/]+)/snapshots/detail", SnapshotsDetailHandler),
+    (BLOCK_STORAGE_BASE_URL + r"/v2/([^/]+)/snapshots/([^/]+)", SnapshotHandler),
+    (BLOCK_STORAGE_BASE_URL + r"/v2/([^/]+)/snapshots/([^/]+)/metadata", SnapshotMetadataHandler),
+    ## (BLOCK_STORAGE_BASE_URL + r"/v2/([^/]+)/snapshots/([^/]+)/action", SnapshotsActionHandler),
     
     ####### new Add handler ############
-    (BLOCK_STORAGE_BASE_URL + r"/v2/(.*)/os-volume-transfer/detail", OsVolumeTransferDetailHandler),
+    (BLOCK_STORAGE_BASE_URL + r"/v2/([^/]+)/os-volume-transfer/detail", OsVolumeTransferDetailHandler),
 
-    (BLOCK_STORAGE_BASE_URL + r"/v2/(.*)/qos-specs", QosSpecsHandler),
+    (BLOCK_STORAGE_BASE_URL + r"/v2/([^/]+)/qos-specs", QosSpecsHandler),
 
-    (BLOCK_STORAGE_BASE_URL + r"/v2/(.*)/types", TypesHandler),
+    (BLOCK_STORAGE_BASE_URL + r"/v2/([^/]+)/types", TypesHandler),
 ]
 
 urls = urls_low_version + urls_v2
