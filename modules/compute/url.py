a31f8d8f09b919de88a7149129ac47f4485cb85b
cw_ali <cwdaydayup@gmail.com>
2016-02-20 00:08:54 +0800

    complete instance start stop reboot action

--- modules/compute/url.py
+++ modules/compute/url.py
@@ -3,12 +3,11 @@ from cloudGate.common.define import *
 
 urls_v2_1 = [
     (COMPUTE_BASE_URL, ComputeBaseHandler),
-    (COMPUTE_BASE_URL + r"/v2.1/(.*)/extensions", ExtensionsHandler),
-    (COMPUTE_BASE_URL + r"/v2.1/(.*)/servers", ServersHandler),
-    #(COMPUTE_BASE_URL + r"/v2.1/(.*)/limits(.*)", ServersHandler),
-    (COMPUTE_BASE_URL + r"/v2.1/(.*)/servers/detail", ServersDetailHandler),
-    (COMPUTE_BASE_URL + r"/v2.1/(.*)/servers/(.*)", ServerHandler),
-    (COMPUTE_BASE_URL + r"/v2.1/(.*)/servers/(.*)/action", ServerActionHandler),
+    (COMPUTE_BASE_URL + r"/v2.1/([^/]+)/servers", ServersHandler), #/v2.1/{tenant_id}/servers
+    (COMPUTE_BASE_URL + r"/v2.1/([^/]+)/servers/detail", ServersDetailHandler), #/v2.1/{tenant_id}/servers/detail
+    (COMPUTE_BASE_URL + r"/v2.1/([^/]+)/servers/(.*)", ServerHandler), #/v2.1/tenant_id}/servers/{server_id}
+    (COMPUTE_BASE_URL + r"/v2.1/([^/]+)/extensions", ExtensionsHandler),
+    (COMPUTE_BASE_URL + r"/v2.1/([^/]+)/servers/([^/]+)/action", ServerActionHandler), #/v2.1/{tenant_id}/servers/{server_id}/action
 ]
 
 #if we support other version api, use urls = urls_v2_1 + urls_other_version
