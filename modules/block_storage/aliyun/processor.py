8da8c38a2889bd3c50b17440c274f81d38406683
wujun <nujuw@126.com>
2016-02-21 02:23:25 +0800

    update block_storage code ... night ... 7

--- modules/block_storage/aliyun/processor.py
+++ modules/block_storage/aliyun/processor.py
@@ -269,7 +269,12 @@ class AliyunBlockStorageProcessor(BlockStorageProcessorBase):
             response = self.clt.do_action(r)
             resp = json.loads(response)
             print "createVolume WUJUN response is ", json.dumps(resp, indent=4)
-            disk_id = resp["RequestId"]
+            ### if not resp.has_key("DiskId"):
+            if resp.has_key("Code"):
+                print "Aliyun Create Disk Operation have Error!!!!!!"
+                return None
+            
+            disk_id = resp["DiskId"]
             
             r = DescribeDisksRequest.DescribeDisksRequest()
             r.set_accept_format('json')
@@ -518,7 +523,7 @@ class AliyunBlockStorageProcessor(BlockStorageProcessorBase):
     def deleteVolume(self, tenant_id, volume_id):
         print "deleteVolume WUJUN begin .... tenant_id is ", tenant_id, "  volume_id is ", volume_id     
         r = DeleteDiskRequest.DeleteDiskRequest()
-        r.set_DiskId(volume_id)
+        ##r.set_DiskId(volume_id)
         r.set_accept_format('json')
         response = self.clt.do_action(r)
         resp = json.loads(response)
