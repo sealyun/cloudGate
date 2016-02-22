dc333b78fa91196e446d523f07003ac71f118215
yjl <yjl@impower-tech.com>
2016-02-22 17:05:29 +0800

    ff

--- modules/networking/httptest.sh
+++ modules/networking/httptest.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
 python httptest.py \
+NetworkTest.test_NetworksHandler_GET \
+NetworkTest.test_NetworksHandler_POST \
+NetworkTest.test_NetworksHandler_GET \
+NetworkTest.test_NetworkHandler_GET \
 NetworkTest.test_NetworkHandler_PUT \
+NetworkTest.test_NetworkHandler_DELETE \
+NetworkTest.test_NetworkHandler_GET \
+NetworkTest.test_NetworksHandler_GET \
+
+
+
+
 
